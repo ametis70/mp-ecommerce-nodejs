d60148541e6e42b3223f43e4b03a11aea06f8261
Ian Mancini <ianmethyst@gmail.com>
2021-11-18 17:49:54 -0300

    Add backend code

--- app.js
+++ app.js
@@ -1,22 +1,63 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mercadopago = require('mercadopago')
 
-var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const BASE_URL = process.env.BASE_URL
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
+mercadopago.configure({
+  integrator_id: process.env.INTEGRATOR_ID,
+  access_token: process.env.ACCESS_TOKEN,
+})
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('assets'))
+
+app.use('/assets', express.static(__dirname + '/assets'))
+
+app.get('/', function (_, res) {
+  res.render('home')
+})
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
-});
+  const { title, price, unit, img } = req.query
 
-app.listen(port);
+  if (!title || !price || !unit || !img) {
+    throw new Error('Missing parameters')
+  }
+
+  const preference = {
+    items: [
+      {
+        id: '1234',
+        title: title,
+        currency_id: 'ARS',
+        unit_price: parseFloat(price),
+        quantity: parseFloat(unit),
+        picture_url: `${BASE_URL}/${img.slice(1, img.length)}`,
+        description: 'Dispositivo móvil de Tienda e-commerce',
+      },
+    ],
+    external_reference: process.env.EMAIL,
+  }
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      console.log(response.body)
+      const { id } = response.body
+
+      res.render('detail', { ...req.query, id })
+    })
+    .catch(function (error) {
+      console.log(error)
+      res.status(500).send('There was an error with MercadoPago')
+    })
+})
+
+app.listen(port)
